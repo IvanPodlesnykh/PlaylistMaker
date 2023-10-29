1b5f923ededd42980ee40236c7c8cee7a1a8002a
IvanPodlesnykh <podlesnihwork@gmail.com>
2023-10-29 19:18:00 +0500

    Приложение Playlist Maker Реализовал главный экран с кнопками перехода на прочие экраны Реализовал экран настроек с возможностью возврата на главный экран Цвета, строки и повторяющиеся размеры вынесены в ресурсы Реализовал возврат с экрана настроек на главный экран через this.finish(), т.к. через startActivity(...) кидает новый гланый экран наверх стека

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.ivanpodlesnykh.playlistmaker"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.ivanpodlesnykh.playlistmaker"
--- app/src/main/java/com/ivanpodlesnykh/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/ivanpodlesnykh/playlistmaker/MainActivity.kt
@@ -1,11 +1,38 @@
 package com.ivanpodlesnykh.playlistmaker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val searchButton = findViewById<Button>(R.id.search_button)
+        val mediaButton = findViewById<Button>(R.id.media_button)
+        val settingsButton = findViewById<Button>(R.id.settings_button)
+
+        val searchButtonClickListener: View.OnClickListener = object : View.OnClickListener {
+            override fun onClick(p0: View?) {
+                val searchIntent = Intent(this@MainActivity, SearchActivity::class.java)
+                startActivity(searchIntent)
+            }
+        }
+
+        searchButton.setOnClickListener(searchButtonClickListener)
+
+        mediaButton.setOnClickListener{
+            val mediaIntent = Intent(this, MediaActivity::class.java)
+            startActivity(mediaIntent)
+        }
+
+        settingsButton.setOnClickListener{
+            val settingsIntent = Intent(this, SettingsActivity::class.java)
+            startActivity(settingsIntent)
+        }
     }
 }
