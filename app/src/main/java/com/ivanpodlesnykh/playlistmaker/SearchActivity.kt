ac821884779c424515e9c96965b8bc465bbf862c
IvanPodlesnykh <podlesnihwork@gmail.com>
2023-11-08 22:02:42 +0500

    Приложение Playlist Maker Второе и третье ДЗ: 1. Добавил действие кнопке "Пользовательское соглашение" на экране настроек 2. Добавил действие кнопке "Написать в поддержку" на экране настроек 3. Реализовал вёрстку экрану поиска

--- app/src/main/java/com/ivanpodlesnykh/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/ivanpodlesnykh/playlistmaker/SearchActivity.kt
@@ -1,11 +1,53 @@
 package com.ivanpodlesnykh.playlistmaker
 
+import android.content.Context
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        val backButton = findViewById<ImageView>(R.id.search_back_button)
+        backButton.setOnClickListener{
+            this.finish()
+        }
+
+        val textInput = findViewById<EditText>(R.id.search_bar)
+
+        val clearButton = findViewById<ImageView>(R.id.clear_search_button)
+        clearButton.visibility = View.GONE
+
+        clearButton.setOnClickListener{
+            textInput.setText("")
+            val view = currentFocus
+            val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+            inputMethodManager?.hideSoftInputFromWindow(view?.windowToken, 0)
+        }
+
+        val textWatcher = object: TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if(s.isNullOrEmpty()) {
+                    clearButton.visibility = View.GONE
+                } else clearButton.visibility = View.VISIBLE
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+
+        }
+        textInput.addTextChangedListener(textWatcher)
     }
 }
--- app/src/main/java/com/ivanpodlesnykh/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/ivanpodlesnykh/playlistmaker/SettingsActivity.kt
@@ -1,8 +1,10 @@
 package com.ivanpodlesnykh.playlistmaker
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.Switch
 import androidx.appcompat.app.AppCompatActivity
 
 class SettingsActivity : AppCompatActivity() {
@@ -15,6 +17,26 @@ class SettingsActivity : AppCompatActivity() {
 //            val mainActivityIntent = Intent(this@SettingsActivity, MainActivity::class.java)
 //            startActivity(mainActivityIntent)
             this.finish()
+        }
+
+        val shareButton = findViewById<ImageView>(R.id.share_app_button)
+        shareButton.setOnClickListener{
+
+        }
+
+        val supportButton = findViewById<ImageView>(R.id.support_button)
+        supportButton.setOnClickListener{
+            val intent = Intent(Intent.ACTION_SENDTO, Uri.parse("mailto:"))
+            intent.putExtra(Intent.EXTRA_EMAIL, arrayOf(getString(R.string.dev_mail)))
+            intent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.mail_to_dev_topic))
+            intent.putExtra(Intent.EXTRA_TEXT, getString(R.string.mail_to_dev_text))
+            startActivity(intent)
+        }
+
+        val userAgreementButton = findViewById<ImageView>(R.id.user_agreement_button)
+        userAgreementButton.setOnClickListener{
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.user_agreement_url)))
+            startActivity(intent)
         }
     }
 }
