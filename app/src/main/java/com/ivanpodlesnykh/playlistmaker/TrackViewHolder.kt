0359cbd5de1be70c34f07c10b7469b518122b333
IvanPodlesnykh <podlesnihwork@gmail.com>
2024-01-11 19:54:41 +0500

    12 Спринт Задание на 12 спринт: 1. Реализовано сохранение и подгрузка темной темы из SharedPreferences 2. Реализовано сохранение треков в SharedPreferences 3. Реализовано отображение истории поиска

--- app/src/main/java/com/ivanpodlesnykh/playlistmaker/TrackViewHolder.kt
+++ app/src/main/java/com/ivanpodlesnykh/playlistmaker/TrackViewHolder.kt
@@ -1,10 +1,13 @@
 package com.ivanpodlesnykh.playlistmaker
 
+import android.content.SharedPreferences
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
@@ -16,6 +19,12 @@ class TrackViewHolder(itemView: View) : ViewHolder(itemView) {
     val artistName = itemView.findViewById<TextView>(R.id.artist_name)
     val trackTime = itemView.findViewById<TextView>(R.id.track_time)
     val trackCover = itemView.findViewById<ImageView>(R.id.track_cover)
+
+    val sharedPreferences = itemView.context.getSharedPreferences("sharedPref",
+        AppCompatActivity.MODE_PRIVATE
+    )
+    val searchHistory = SearchHistory(sharedPreferences)
+
     fun bind(track: Track) {
         songName.text = track.trackName
         artistName.text = track.artistName
@@ -28,5 +37,9 @@ class TrackViewHolder(itemView: View) : ViewHolder(itemView) {
             .fitCenter()
             .transform(RoundedCorners(itemView.resources.getDimensionPixelSize(R.dimen.track_cover_corner_radius)))
             .into(trackCover)
+
+        itemView.setOnClickListener{
+            searchHistory.saveTrackToList(track)
+        }
     }
 }
